fc8ae5538326174b9f39c9c551d1b3c82de99374
Frank MARSHALL <frank@readresolve.io>
2024-01-20 19:22:16 +0100

    Updated with multidim array examples

--- pure-js/arrays.js
+++ pure-js/arrays.js
@@ -1,13 +1,28 @@
 const students = ["Sab","Thao","Em"];
 let first = students[0]; // Read at index 0
-console.log(first);
+//console.log(first);
 students[0] = "Emir"; // Write at index 0
-console.log(first); // ???
+//console.log(first); // ???
 first = students[0]; // Reassign first
-console.log(first); //
+//console.log(first); //
 
 const fruits = []; // Empty array
-console.log(fruits[0]);
+//console.log(fruits[0]);
 fruits[0] = "Apple"; // Dynamic sizing
-console.table(fruits);
+//console.table(fruits);
+fruits[1] = "Mel";
+fruits[3] = "Toto";
+//console.log(fruits);
 
+// Multidimensional arrays (2d, 3d, 4d...)
+// An array containing other arrays 
+const twoDimArray = [];
+twoDimArray[0] = ["Apple", "Peach"];
+twoDimArray[1] = ["Fish", "Meat", 1212654];
+twoDimArray[2] = ["Fanta", "Coca", true];
+twoDimArray[4] = "Boom";
+console.log(twoDimArray);
+const apple = twoDimArray[0][0];
+const meat = twoDimArray[1][1];
+console.log(`[0][0]=${apple}`);
+console.log(`[1][1]=${meat}`);
