2b48d51ee50bdbf145a07d634b22524fd39ce38b
Frank MARSHALL <frank@readresolve.io>
2024-02-06 09:53:14 +0100

    Implemented fizzbuzz

--- itp4-cli/src/commands/fb.js
+++ itp4-cli/src/commands/fb.js
@@ -1,9 +1,21 @@
 import { parseToInt } from '../utilities/parsers.js';
+import os from 'os';
 
 function execute(args) {
-    const number = parseToInt(args[0]);
-    // Implement algo here:
-    return number;
+    const max = parseToInt(args[0]);
+    let result = '';
+    for (let i = 1; i <= max; i++) {
+        if (i % 15 === 0) { // 15 = 3 * 5
+            result += 'FizzBuzz' + os.EOL;
+        } else if (i % 3 === 0) {
+            result += 'Fizz' + os.EOL;
+        } else if (i % 5 === 0) {
+            result += 'Buzz' + os.EOL
+        } else {
+            result += i + os.EOL;
+        }
+    }
+    return result;
 }
 
 export default execute;
